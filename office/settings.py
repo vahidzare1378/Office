be5330abab2346593bdf8d0d32d91f161ddcfe1f
vahidzare1378 <vahidzareeee1378@gmail.com>
2022-01-01 18:39:53 +0330

    add .env for SERCRET_KEY

--- office/settings.py
+++ office/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#54_vi7+f)9n=%8rem(+^_$zq*pewoencw*zqf-r=t3c5d+9@v'
+SECRET_KEY = Config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
